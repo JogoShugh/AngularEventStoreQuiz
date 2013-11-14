2a5718245633913fa75e8a82de5c8226efcfe942
Josh Gough <jsgough@gmail.com>
2013-11-14 07:42:53 -0500

    " to '

--- app.coffee
+++ app.coffee
@@ -1,7 +1,7 @@
 (->
-  quizApp = angular.module("quizApp", ["ui.bootstrap"])
+  quizApp = angular.module('quizApp', ['ui.bootstrap'])
 
-  serverRoot = "http://localhost:2113"
+  serverRoot = 'http://localhost:2113'
 
   choice = (text, correct) ->
     correct = false if correct isnt true 
@@ -62,7 +62,7 @@
     $scope.questionNewAdd = ->
       questionNew =
         id: questionId++
-        question: "Question?"
+        question: 'Question?'
         choices: [
         ]
       $scope.questions.push questionNew
@@ -73,21 +73,21 @@
     $scope.questionTextRevert = (question) ->
       $scope.questionTextPending.value = question.question
 
-  quizApp.controller "quizController", ($rootScope, $scope, $http, Poller, questions) ->
+  quizApp.controller 'quizController', ($rootScope, $scope, $http, Poller, questions) ->
     $scope.questions = questions
     getAnswerStreamUri = (lastUri, result) ->
       if not lastUri? or not result?
-        return serverRoot + "/streams/answers/head/backward/20?embed=content"
+        return serverRoot + '/streams/answers/head/backward/20?embed=content'
       else
-        return result.data.links[4].uri + "?embed=content"  if result.data.links[4].relation is "previous"
+        return result.data.links[4].uri + '?embed=content'  if result.data.links[4].relation is 'previous'
       lastUri
     updateAnswersArray = (result) ->
       $scope.answerStream.unshift(result.data.entries...)
 
-    $scope.userName = value: "Guest" + (Math.floor(Math.random() * 100000))
+    $scope.userName = value: 'Guest' + (Math.floor(Math.random() * 100000))
     
     pollCurrentAnswers = Poller(2000, ->
-      serverRoot + "/projection/QuestionAnswerCoutns2/state"
+      serverRoot + '/projection/QuestionAnswerCoutns2/state'
     )
     $scope.currentAnswers = pollCurrentAnswers.data
     
@@ -113,7 +113,7 @@
       for question in $scope.questions
         answer =
           eventId: uuid.v4()
-          eventType: "Answer"
+          eventType: 'Answer'
           data:
             user: $scope.userName
             answer:
@@ -125,11 +125,11 @@
             answer.data.answer.correct = choice.correct
         answers.push answer
 
-      $http.post(serverRoot + "/streams/answers", answers).success ->
-        $scope.successMessage = "Success!"
+      $http.post(serverRoot + '/streams/answers', answers).success ->
+        $scope.successMessage = 'Success!'
         $scope.quizVisible = false
 
-  quizApp.factory "Poller", ($http, $timeout) ->
+  quizApp.factory 'Poller', ($http, $timeout) ->
     createPoller = (interval, getUri, callback) ->
       data =
         response: {}   
@@ -146,73 +146,73 @@
 
   questions = [
     id: 1
-    question: "How should I inject a foo into a blah"
+    question: 'How should I inject a foo into a blah'
     choices: [
-      choice("You should use constructor injection always"), 
-      choice("Consider using a functional style that passes your depdendencies via a closure", true), 
-      choice("Use a container that lets you use any kind of injection you like!")
+      choice('You should use constructor injection always'), 
+      choice('Consider using a functional style that passes your depdendencies via a closure', true), 
+      choice('Use a container that lets you use any kind of injection you like!')
     ]
   ,
     id: 2
-    question: "What style of programming do you need to use when you have lots of attributes on top of a class or method, for things like [Logged] [Transactional] [Requires(Role.Admin)]"
+    question: 'What style of programming do you need to use when you have lots of attributes on top of a class or method, for things like [Logged] [Transactional] [Requires(Role.Admin)]'
     choices: [
-      choice("Aspect-oriented programming", true),
-      choice("Magic-oriented programming"),
-      choice("Functional programming")
+      choice('Aspect-oriented programming', true),
+      choice('Magic-oriented programming'),
+      choice('Functional programming')
     ]
   ,
     id: 3
-    question: "Why do you have to declare properties as virtual when you use tools that create dynamic proxies (like NHibernate)?"
+    question: 'Why do you have to declare properties as virtual when you use tools that create dynamic proxies (like NHibernate)?'
     choices: [
-      choice("Language flaw"),
-      choice("Because a class gets created at run-time that derives from your class to stand in its place", true),
-      choice("Because you need to create a derived class in the code-behind next")
+      choice('Language flaw'),
+      choice('Because a class gets created at run-time that derives from your class to stand in its place', true),
+      choice('Because you need to create a derived class in the code-behind next')
     ]
   ,
     id: 4
     question: "When you have a method that takes several parameters, what's the name of the common refactoring that lets you generalize to a single parameter?"
     choices: [
-      choice("Pull up method"),
-      choice("Extract interface"),
-      choice("Refactor parameters to object", true)
+      choice('Pull up method'),
+      choice('Extract interface'),
+      choice('Refactor parameters to object', true)
     ]
   ,
     id: 5
     question: "Which framework for CQRS in the .NET world went from a 'framework' to a 'reference application' (aka dead project)?"
     choices: [
-      choice("FCQRS"),
-      choice("C#QRS"),
-      choice("NCQRS", true)
+      choice('FCQRS'),
+      choice('C#QRS'),
+      choice('NCQRS', true)
     ]
   ,
     id: 6
-    question: "When should you prefer a pull model versus a push model"
+    question: 'When should you prefer a pull model versus a push model'
     choices: [
-      choice("When an inventory system needs to know about recent sales")
-      choice("When building a large-scale searchable index", true)
-      choice("When availability is more important than consistency")
+      choice('When an inventory system needs to know about recent sales')
+      choice('When building a large-scale searchable index', true)
+      choice('When availability is more important than consistency')
     ]
   ,
     id: 7
-    question: "What is something that developers should try to do more often?"
+    question: 'What is something that developers should try to do more often?'
     choices: [
-      choice("Make systems that are able to recover from every failure")
-      choice("Use eventual consistency when users complain about latency")
-      choice("Recognize when a technical problem should be seen as a business problem that might need a business solution, not a technical solution", true)      
+      choice('Make systems that are able to recover from every failure')
+      choice('Use eventual consistency when users complain about latency')
+      choice('Recognize when a technical problem should be seen as a business problem that might need a business solution, not a technical solution', true)      
     ]
   ,
     id: 8
-    question: "What phrases may help business people feel comfortable with latency between parts of your system?"
+    question: 'What phrases may help business people feel comfortable with latency between parts of your system?'
     choices: [
-      choice("When you make a sale in the sales system, your data will be eventually consistent in the billing system")
-      choice("When you make a sale in the sales system, the computer may have to think a while before it shows up in billing. Call us if it takes too long.", true)
-      choice("When you make a sale in the sales system, remember the CAP theorem: we can only guarantee any two of the following properties: Consistency, Availability, and Partition tolerence at any one time. Therefore, we cannot guarantee that sales and billing will always be in sync")      
+      choice('When you make a sale in the sales system, your data will be eventually consistent in the billing system')
+      choice('When you make a sale in the sales system, the computer may have to think a while before it shows up in billing. Call us if it takes too long.', true)
+      choice('When you make a sale in the sales system, remember the CAP theorem: we can only guarantee any two of the following properties: Consistency, Availability, and Partition tolerence at any one time. Therefore, we cannot guarantee that sales and billing will always be in sync')      
     ]
   ,
     id: 9
     question: "When you do have eventual consistency between system components, what's an acceptable user experience?"
     choices: [
-      choice("Redirect the user back to the screen to read the data they just updated immediately")
+      choice('Redirect the user back to the screen to read the data they just updated immediately')
       choice("Have William Shatner in Captain Kirk garb tell your user they are beaming the data into the servers. Use AJAX to poll the server for a response, and keep repeating Kirk in a loop until it's done, and then show the updated data")
       choice("Lie to them. Tell them it's already done.", true)
     ]
--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 (function() {
   (function() {
     var choice, questions, quizApp, serverRoot;
-    quizApp = angular.module("quizApp", ["ui.bootstrap"]);
-    serverRoot = "http://localhost:2113";
+    quizApp = angular.module('quizApp', ['ui.bootstrap']);
+    serverRoot = 'http://localhost:2113';
     choice = function(text, correct) {
       if (correct !== true) {
         correct = false;
@@ -94,7 +94,7 @@
         var questionNew;
         questionNew = {
           id: questionId++,
-          question: "Question?",
+          question: 'Question?',
           choices: []
         };
         return $scope.questions.push(questionNew);
@@ -106,15 +106,15 @@
         return $scope.questionTextPending.value = question.question;
       };
     });
-    quizApp.controller("quizController", function($rootScope, $scope, $http, Poller, questions) {
+    quizApp.controller('quizController', function($rootScope, $scope, $http, Poller, questions) {
       var getAnswerStreamUri, pollAnswerStream, pollCurrentAnswers, updateAnswersArray;
       $scope.questions = questions;
       getAnswerStreamUri = function(lastUri, result) {
         if ((lastUri == null) || (result == null)) {
-          return serverRoot + "/streams/answers/head/backward/20?embed=content";
+          return serverRoot + '/streams/answers/head/backward/20?embed=content';
         } else {
-          if (result.data.links[4].relation === "previous") {
-            return result.data.links[4].uri + "?embed=content";
+          if (result.data.links[4].relation === 'previous') {
+            return result.data.links[4].uri + '?embed=content';
           }
         }
         return lastUri;
@@ -124,10 +124,10 @@
         return (_ref = $scope.answerStream).unshift.apply(_ref, result.data.entries);
       };
       $scope.userName = {
-        value: "Guest" + (Math.floor(Math.random() * 100000))
+        value: 'Guest' + (Math.floor(Math.random() * 100000))
       };
       pollCurrentAnswers = Poller(2000, function() {
-        return serverRoot + "/projection/QuestionAnswerCoutns2/state";
+        return serverRoot + '/projection/QuestionAnswerCoutns2/state';
       });
       $scope.currentAnswers = pollCurrentAnswers.data;
       $scope.answerStream = [];
@@ -165,7 +165,7 @@
           question = _ref[_i];
           answer = {
             eventId: uuid.v4(),
-            eventType: "Answer",
+            eventType: 'Answer',
             data: {
               user: $scope.userName,
               answer: {
@@ -184,13 +184,13 @@
           }
           answers.push(answer);
         }
-        return $http.post(serverRoot + "/streams/answers", answers).success(function() {
-          $scope.successMessage = "Success!";
+        return $http.post(serverRoot + '/streams/answers', answers).success(function() {
+          $scope.successMessage = 'Success!';
           return $scope.quizVisible = false;
         });
       };
     });
-    quizApp.factory("Poller", function($http, $timeout) {
+    quizApp.factory('Poller', function($http, $timeout) {
       var createPoller;
       createPoller = function(interval, getUri, callback) {
         var data, poller, uri;
@@ -218,40 +218,40 @@
     questions = [
       {
         id: 1,
-        question: "How should I inject a foo into a blah",
-        choices: [choice("You should use constructor injection always"), choice("Consider using a functional style that passes your depdendencies via a closure", true), choice("Use a container that lets you use any kind of injection you like!")]
+        question: 'How should I inject a foo into a blah',
+        choices: [choice('You should use constructor injection always'), choice('Consider using a functional style that passes your depdendencies via a closure', true), choice('Use a container that lets you use any kind of injection you like!')]
       }, {
         id: 2,
-        question: "What style of programming do you need to use when you have lots of attributes on top of a class or method, for things like [Logged] [Transactional] [Requires(Role.Admin)]",
-        choices: [choice("Aspect-oriented programming", true), choice("Magic-oriented programming"), choice("Functional programming")]
+        question: 'What style of programming do you need to use when you have lots of attributes on top of a class or method, for things like [Logged] [Transactional] [Requires(Role.Admin)]',
+        choices: [choice('Aspect-oriented programming', true), choice('Magic-oriented programming'), choice('Functional programming')]
       }, {
         id: 3,
-        question: "Why do you have to declare properties as virtual when you use tools that create dynamic proxies (like NHibernate)?",
-        choices: [choice("Language flaw"), choice("Because a class gets created at run-time that derives from your class to stand in its place", true), choice("Because you need to create a derived class in the code-behind next")]
+        question: 'Why do you have to declare properties as virtual when you use tools that create dynamic proxies (like NHibernate)?',
+        choices: [choice('Language flaw'), choice('Because a class gets created at run-time that derives from your class to stand in its place', true), choice('Because you need to create a derived class in the code-behind next')]
       }, {
         id: 4,
         question: "When you have a method that takes several parameters, what's the name of the common refactoring that lets you generalize to a single parameter?",
-        choices: [choice("Pull up method"), choice("Extract interface"), choice("Refactor parameters to object", true)]
+        choices: [choice('Pull up method'), choice('Extract interface'), choice('Refactor parameters to object', true)]
       }, {
         id: 5,
         question: "Which framework for CQRS in the .NET world went from a 'framework' to a 'reference application' (aka dead project)?",
-        choices: [choice("FCQRS"), choice("C#QRS"), choice("NCQRS", true)]
+        choices: [choice('FCQRS'), choice('C#QRS'), choice('NCQRS', true)]
       }, {
         id: 6,
-        question: "When should you prefer a pull model versus a push model",
-        choices: [choice("When an inventory system needs to know about recent sales"), choice("When building a large-scale searchable index", true), choice("When availability is more important than consistency")]
+        question: 'When should you prefer a pull model versus a push model',
+        choices: [choice('When an inventory system needs to know about recent sales'), choice('When building a large-scale searchable index', true), choice('When availability is more important than consistency')]
       }, {
         id: 7,
-        question: "What is something that developers should try to do more often?",
-        choices: [choice("Make systems that are able to recover from every failure"), choice("Use eventual consistency when users complain about latency"), choice("Recognize when a technical problem should be seen as a business problem that might need a business solution, not a technical solution", true)]
+        question: 'What is something that developers should try to do more often?',
+        choices: [choice('Make systems that are able to recover from every failure'), choice('Use eventual consistency when users complain about latency'), choice('Recognize when a technical problem should be seen as a business problem that might need a business solution, not a technical solution', true)]
       }, {
         id: 8,
-        question: "What phrases may help business people feel comfortable with latency between parts of your system?",
-        choices: [choice("When you make a sale in the sales system, your data will be eventually consistent in the billing system"), choice("When you make a sale in the sales system, the computer may have to think a while before it shows up in billing. Call us if it takes too long.", true), choice("When you make a sale in the sales system, remember the CAP theorem: we can only guarantee any two of the following properties: Consistency, Availability, and Partition tolerence at any one time. Therefore, we cannot guarantee that sales and billing will always be in sync")]
+        question: 'What phrases may help business people feel comfortable with latency between parts of your system?',
+        choices: [choice('When you make a sale in the sales system, your data will be eventually consistent in the billing system'), choice('When you make a sale in the sales system, the computer may have to think a while before it shows up in billing. Call us if it takes too long.', true), choice('When you make a sale in the sales system, remember the CAP theorem: we can only guarantee any two of the following properties: Consistency, Availability, and Partition tolerence at any one time. Therefore, we cannot guarantee that sales and billing will always be in sync')]
       }, {
         id: 9,
         question: "When you do have eventual consistency between system components, what's an acceptable user experience?",
-        choices: [choice("Redirect the user back to the screen to read the data they just updated immediately"), choice("Have William Shatner in Captain Kirk garb tell your user they are beaming the data into the servers. Use AJAX to poll the server for a response, and keep repeating Kirk in a loop until it's done, and then show the updated data"), choice("Lie to them. Tell them it's already done.", true)]
+        choices: [choice('Redirect the user back to the screen to read the data they just updated immediately'), choice("Have William Shatner in Captain Kirk garb tell your user they are beaming the data into the servers. Use AJAX to poll the server for a response, and keep repeating Kirk in a loop until it's done, and then show the updated data"), choice("Lie to them. Tell them it's already done.", true)]
       }
     ];
     return quizApp.constant('questions', questions);
